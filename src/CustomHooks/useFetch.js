cebcd77843c1d536c7d6baec60cf53169697e3ca
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-24 20:17:37 +0100

    Update useFetch.js

--- src/CustomHooks/useFetch.js
+++ src/CustomHooks/useFetch.js
@@ -1,1 +1,37 @@
+import { useState, useEffect } from "react";
 
+const useFetch = (url) => {
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+  const [apiData, setApiData] = useState(null);
+
+  // console.log(url);
+  // console.log(data);
+
+  const fetchData = () => {
+    if (typeof url !== "string" || url.length === 0) {
+      throw new Error("Invalid URL");}
+    setLoading(true)
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setApiData(data))
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  
+  useEffect(() => {
+        fetchData();
+  }, []);
+
+  return { loading, error, apiData };
+};
+
+export default useFetch;
+
+// const { loading, error, apiData } = useFetch(url);
+// return (<>{loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log({apiData})}</div>)}</>)
+// map: <ul>{path.map((item, index) => (<li key={index}>{item}</li>))}</ul>
